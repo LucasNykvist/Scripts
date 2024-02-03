2d66bbafba25022e0cae2bcf9448c70a28e64735
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-02-03 12:09:33 +0100

    Refactor shutdown script to improve user experience

--- Shutdown.ps1
+++ Shutdown.ps1
@@ -1,20 +1,33 @@
-$delayInMinutes = Read-Host -Prompt "Enter the delay in minutes before shutdown"
+$delayText = "Enter the delay in minutes before shutdown"
+$invalidInputText = "Invalid input. Please enter a valid number"
+$delayInMinutes = Read-Host -Prompt $delayText
+$confirmationTextMinute = "Are you sure you want to shutdown the computer in $delayInMinutes minute (y/n)"
+$confirmationTextMinutes = "Are you sure you want to shutdown the computer in $delayInMinutes minutes (y/n)"
+$cancelledText = "Shutdown cancelled"
+
+
 
 if(![int]::TryParse($delayInMinutes, [ref]0))
 {
-    Write-Host "Invalid input. Please enter a valid number"
+    Write-Host $invalidInputText
     exit
 }
 
 $delayInSeconds = $delayInMinutes * 60
 
-$confirmation = Read-Host -Prompt "Are you sure you want to shutdown the computer in $delayInMinutes minute(s) (y/n)"
+if($delayInMinutes -lt 2)
+{
+    $confirmation = Read-Host -Prompt $confirmationTextMinute
+} else 
+{
+    $confirmation = Read-Host -Prompt $confirmationTextMinutes
+}
 
 if($confirmation -eq "y")
 {
     shutdown /s /t $delayInSeconds
 } else 
 {
-    Write-Host "Shutdown cancelled"
+    Write-Host $cancelledText
 }
 
